88bc79c09daeae828904506159a7262eb5bcd693
Alvira Narshidani <Alvira.Narshidani@GeorgianCollege.ca>
2019-10-09 17:42:48 -0400

    Add project files.

---
+++ OnlineOpticalShoppingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineOpticalShoppingSystem.Startup))]
+namespace OnlineOpticalShoppingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
