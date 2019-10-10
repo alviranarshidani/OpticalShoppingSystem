6f540d35ab8f9e40c1415414a552f551bbbb6eb3
Alvira Narshidani <Alvira.Narshidani@GeorgianCollege.ca>
2019-10-09 20:59:34 -0400

    controllers & views

---
+++ OnlineOpticalShoppingSystem/Controllers/Application/ContactLensController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineOpticalShoppingSystem.Controllers.Application
+{
+    public class ContactLensController : Controller
+    {
+        // GET: ContactLens
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineOpticalShoppingSystem/Controllers/Application/GoggelsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineOpticalShoppingSystem.Controllers.Application
+{
+    public class GoggelsController : Controller
+    {
+        // GET: Goggels
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
